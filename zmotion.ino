ac2c971564b4cea6c3c942d6827aeaa8baf40a20
Jonathan Morrice <jonny@morrices.de>
2013-02-10 16:12:40 +0000

    Fixed zmotion debug function

--- zmotion.ino
+++ zmotion.ino
@@ -13,19 +13,18 @@ void zilog_init()
 void zilog_debug()
 {
   if (zilog.available())
-    Serial.print(zilog.read());
+    Serial.print((char)zilog.read());
   if (Serial.available())
-    zilog.print(Serial.read());
+    zilog.print((char)Serial.read());
 }
 
 boolean zilog_detect_motion()
 {
   zilog.print("a");
-  delay(2);
+  //delay(2);
   if(zilog.available())
   {
     char c = zilog.read();
-    Serial.println(c);
     if(c == 'Y')
       return true;
     else
