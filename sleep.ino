703e3fd2e0a3697f3acf258f98bbde6cf737c210
Jonathan Morrice <jonny@morrices.de>
2013-02-28 14:46:33 +0000

    Cleaned Structure

--- sleep.ino
+++
@@ -1,8 +0,0 @@
-#include <LowPower.h>
-
-void avr_sleep()
-{
-  LowPower.powerDown(SLEEP_4S, //change according to motion sample period
-                     ADC_OFF, 
-                     BOD_OFF);
-}
